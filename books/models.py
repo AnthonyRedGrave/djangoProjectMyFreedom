204ea111169cc2197f70c8e830c9e44d0936647f
Барнев Олег Леонидович <o.barnev@comita.digital>
2023-05-26 21:52:39 +0300

    отношение между жанром и книгами

--- books/models.py
+++ books/models.py
@@ -5,6 +5,11 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     year = models.IntegerField()
+    raiting = models.IntegerField(default=0)
+
+    publisher = models.CharField(max_length=50, null=True)
+
+    genre = models.ForeignKey("Genre", on_delete=models.DO_NOTHING, null=True, blank=True, related_name='books')
 
     def __str__(self):
         # строковое представление объекта
@@ -13,3 +18,7 @@ class Book(models.Model):
 
 class Genre(models.Model):
     title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Жанр: {self.id}, {self.title}"
+
