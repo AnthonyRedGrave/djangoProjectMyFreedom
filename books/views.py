576dd173ddfac321e39ab0e1a77c2a54693e94f9
Барнев Олег Леонидович <o.barnev@comita.digital>
2023-05-30 21:49:13 +0300

    return genres in html

--- books/views.py
+++ books/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Genre
 
 
 def books(request):
     books = Book.objects.all()
+    genres = Genre.objects.all()
     return render(request, "index.html", context={"books": books,
-                                                  "name": "Олег"})
+                                                  "genres": genres})
 
